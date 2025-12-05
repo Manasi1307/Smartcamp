a311cfe6457712e80c44c0444cc7f4571a911e64
manasi <mansitambade59@gmail.com>
2025-12-05 18:41:52 +0530

    initial Commit

---
+++ Header.php
@@ -0,0 +1,13 @@
+<nav class="cc-navbar">
+        <div class="cc-logo-section">
+            <img src="/Assets/Logo.png" class="cc-logo-icon" alt="logo">
+            <span class="cc-logo-text">SmartCampus</span>
+        </div>
+
+        <ul class="cc-nav-links">
+            <li><a href="" class="cc-active-link">Home</a></li>
+            <li><a href="/about.html">About</a></li>
+            <li><a href="">Modules</a></li>
+            <li><a href="">Contact Us</a></li>
+        </ul>
+    </nav>
